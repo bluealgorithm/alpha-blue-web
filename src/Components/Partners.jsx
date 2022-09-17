abf8366f1295158c0201c093264f505bbfa8f9ce
femmyte <sanyaoluadefemi@gmail.com>
2022-09-17 14:23:21 +0100

    designed our Partner component

--- src/Components/Partners.jsx
+++ src/Components/Partners.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import afrilab from '../asset/images/afrilab.png'
+import alpha_blue from '../asset/images/alpa_blue.png'
 const Partners = () => {
   return (
-    <div>Partners</div>
+    <div className='h-[360px] '>
+      <h1 className='font-[600] text-[40px] mt-[56px] text-center mb-[84px]'>PARTNERS</h1>
+      <div className="flex items-center justify-center">
+        <img src={afrilab} alt="afrilab logo" />
+        <img src={alpha_blue} alt="alpha_blue logo" />
+      </div>
+    </div>
   )
 }
 
