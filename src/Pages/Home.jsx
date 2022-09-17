840fdc9338e4bb445c0ce0bb027eb6db62ccde80
femmyte <sanyaoluadefemi@gmail.com>
2022-09-17 14:24:24 +0100

    used the component to build the Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Nav from '../Components/Nav'
+import Header from '../Components/Header'
+import About from '../Components/About'
+import Mission_Vision from '../Components/Mission_Vision'
+import Difference from '../Components/Difference'
+import Partners from '../Components/Partners'
+import Contact from '../Components/Contact'
 
 const Home = () => {
   return (
     <div>
-        <Nav />
+          <Nav />
+          <Header />
+          <About />
+          <Mission_Vision />
+          <Difference />
+          <Partners />
+          <Contact />
     </div>
   )
 }
